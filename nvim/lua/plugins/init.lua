80d58238a17a753dd7e2a49d613e2df78bee4a82
gordo canchola <gordo@g-at-work.home>
2023-04-13 21:51:23 -0700

    Adding nvim sudoku

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -5,6 +5,13 @@ return {
   'f-person/git-blame.nvim',
   --'seandewar/nvimesweeper',
   {
+    "jim-fx/sudoku.nvim",
+    cmd = "Sudoku",
+    config = function ()
+      require("sudoku").setup {}
+    end
+  },
+  {
     'dracula/vim',
     name = 'dracula',
     config = function()
