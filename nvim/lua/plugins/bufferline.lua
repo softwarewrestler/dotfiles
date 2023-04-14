0bf8d2e7cc510463eac8d60c77de754fc530044e
gordo canchola <gordo@g-at-work.home>
2023-04-14 15:21:58 -0700

    Adding bufferline and removing cokeline

---
+++ nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,25 @@
+return {
+  "akinsho/bufferline.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  keys = {
+    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
+  },
+  opts = {
+    options = {
+      diagnostics = "nvim_lsp",
+      always_show_bufferline = false,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Neo-tree",
+          highlight = "Directory",
+          text_align = "left",
+        },
+      },
+    },
+  },
+}
